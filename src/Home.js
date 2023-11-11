62b5e0204e044b1e63827205f9039957c0f9d318
Priyanshu-Roushan <priyanshuroushan.cse2023@ritroorkee.com>
2023-11-11 21:31:46 +0530

    final

--- src/Home.js
+++ src/Home.js
@@ -87,88 +87,36 @@ function Home(){
                         <img src={tseries} height={'30px'} width={'30px'}></img>
                         <span class="side-channel-label">T-Series</span>
                     </button>
+                </div>    
+                <div className="main-right">     
+                <div className="main-right-top">
+                            <div className='icons1'>All</div>
+                            <div className='icons2'>Music</div>
+                            <div className='icons3'>Python</div>
+                            <div className='icons4'>Live</div>
+                            <div className='icons5'>Computer Science</div>
+                            <div className='icons6'>Bolliwood Music</div>
+                            <div className='icons7'>Python</div>
+                            <div className='icons8'>Live</div>
+                            <div className='icons15'>Explorin</div>
+                            <div className='icons9'>Tamil Cinema</div>
+                            <div className='icons10'>Movies</div>
+                            <div className='icons11'>CID</div>
+                            <div className='icons12'>Indian Pop-Music</div>
+                            <div className='icons13'>Jee Mains</div>
+                            <div className='icons14'>RIT Roorkee</div>
+                            <div className='icon-scroll'></div>
                 </div>
                 <div class="main-right independent-scroll">
+
                     {videosJson.map((video) => {
                         return <VideoGrid myVideo={video}/>
                     })}
+
+                </div>
                 </div>
             </div>
 
-            {/* <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><hr class="dropdown-divider" /></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" ></input>
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                    </div>
-                </div>
-                </nav> */}
-            {/* <h3>Home</h3> */}
-            {/* <table className="tablename">
-                <tr id="myid">
-                    <th>Company</th>
-                    <th>Contact</th>
-                    <th>Country</th>
-                </tr>
-                <tr>
-                    <td>Alfreds Futterkiste</td>
-                    <td>Maria Anders</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Centro comercial Moctezuma</td>
-                    <td>Francisco Chang</td>
-                    <td>Mexico</td>
-                </tr>
-                <tr>
-                    <td>Ernst Handel</td>
-                    <td>Roland Mendel</td>
-                    <td>Austria</td>
-                </tr>
-                <tr>
-                    <td>Island Trading</td>
-                    <td>Helen Bennett</td>
-                    <td>UK</td>
-                </tr>
-                <tr>
-                    <td>Laughing Bacchus Winecellars</td>
-                    <td>Yoshi Tannamuri</td>
-                    <td>Canada</td>
-                </tr>
-                <tr>
-                    <td>Magazzini Alimentari Riuniti</td>
-                    <td>Giovanni Rovelli</td>
-                    <td>Italy</td>
-                </tr>
-            </table> */}
         </div>
     )
 }
